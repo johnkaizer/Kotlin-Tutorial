d3fc3c4935aee6be0bf30ff48401a80fc8e64a58
john kaizer <johnkaiser964@gmail.com>
2021-08-09 16:36:17 +0300

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,43 @@
+
+//variables
+//val color: String ="brown"
+//fun main() {
+//    val name: String ="john"
+//
+//    var id :Int =20
+//    id=780
+//
+//    println(name)
+//    println(id)
+//    println(color)
+//}
+//var greetings: String? =null
+//fun main() {
+//    if (greetings !=null){
+//        println(greetings)
+//
+//    }else {
+//        println("hi")
+//    }
+//var greetings: String? =null
+//fun main() {
+//    greetings="hello"
+//    when (greetings){
+//        null -> println("hi")
+//        else -> println("nothing here")
+//
+//}
+//}
+//var greetings: String? =null
+//fun main() {
+// val greetingsToPrint= if (greetings !=null)greetings else "hi"
+//    println(greetingsToPrint)
+//}
+
+fun getGreeting(): String {
+    return "Hello Kot"
+}
+fun main() {
+    println("Hello ")
+    print(getGreeting())
+}
